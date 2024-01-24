f1a1c179a981ef2e6e66055a06ae162a680535de
Moita <marcos.sales97@gmail.com>
2024-01-24 16:43:14 -0300

    Create and style home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,68 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
 
-export default function Home() {
+import { Navbar } from '@/components';
+import '@/styles/styles.scss';
+import Image from 'next/image';
+import { BiLogoLinkedin, BiLogoGithub } from 'react-icons/bi';
+import bgImg from '@/assets/bg-img.png';
+
+const Home = () => {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <Navbar />
+      <section className="hero">
+        <div className="container">
+          <div className="row">
+            <div className="col col-12">
+              <h1 className="header-hero">
+                Your <span className="italic">fr</span>iendly neighborhood
+              </h1>
+              <h1 className="header-hero">
+                <span className="italic">Fr</span>ont-end Developer, Moita!
+              </h1>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col col-3">
+              <small>
+                Bringing your ideas
+                <br />
+                to digital reality since 2019.
+              </small>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col col-10">
+              <div className="welcome-box">
+                <Image fill={true} src={bgImg} alt="welcome" />
+              </div>
+            </div>
+            <div className="col col-2">
+              <div className="smaller-box">
+                <div className="inner-box">
+                  <a
+                    href="https://github.com/moitadev"
+                    target="_blank"
+                    className="link-absolute"
+                  ></a>
+                  <BiLogoGithub />
+                </div>
+                <div className="inner-box">
+                  <a
+                    href="https://www.linkedin.com/in/msales97/"
+                    target="_blank"
+                    className="link-absolute"
+                  ></a>
+                  <BiLogoLinkedin />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </section>
+      <div className="noise"></div>
+    </>
   );
-}
+};
+
+export default Home;
